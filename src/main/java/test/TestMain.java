6f5ee53d1ef420b74179f8ed5298138693cafc43
Lukas Trautner <lukas.peter.trautner@uni-weimar.de>
2019-04-30 17:26:13 +0200

    initialized java project

---
+++ src/main/java/test/TestMain.java
@@ -0,0 +1,13 @@
+package test;
+
+import de.aitools.ie.uima.analysis.categorization.AbstractWekaTextClassifier;
+
+public class TestMain {
+
+	@SuppressWarnings("null")
+	public static void main(String[] args) {
+		AbstractWekaTextClassifier test = null;
+		test.hashCode();
+	}
+
+}
