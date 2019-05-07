5016c3fdc01e52b238d0aaf4319a699b146f0637
Ruta Hareshbhai Bakhda <ruta.hareshbhai.bakhda@uni-weimar.de>
2019-05-07 10:30:30 +0200

    Update remove-cliche.py

--- data-preprocessing/remove-cliche.py
+++ data-preprocessing/remove-cliche.py
@@ -5,32 +5,36 @@ import re
 import os
 from os import path
 import string
+from typing import Any
+
 rem = string.punctuation
 pattern = r"[{}]".format(rem)
 
 class Preprocessing():
+    basepath = os.path.dirname(os.path.abspath(__file__))
+    datapath_sbm = basepath + "/lipad/statement-by-members/combined-all-csv/"
+    datapath_oq = basepath + "/lipad/statement-by-members/combined-all-csv/"
+
     def __init__(self):
         super().__init__()
-
         #task 1 : find most common words
-        basepath = path.dirname(__file__)
-        datapath-sbm = basepath+"/lipad/statement-by-members/combined-all-csv/"
-        datapath-oq = basepath+"/lipad/statement-by-members/combined-all-csv/"
-        self.find_cliche(datapath-sbm,'combined.csv')
-        self.find_cliche(datapath-oq,'combined.csv')
+        self.find_cliche(self.datapath_sbm,'combined.csv')
+        self.find_cliche(self.datapath_oq,'combined.csv')
+
+    def read_csv(self,datapath,filename):
+        read_csv_data = pd.read_csv(datapath+filename,delimiter=',' ,header=0)
+        return read_csv_data
 
     def find_cliche(self,datapath,filename):
-        
-        #datapath = basepath+"/lipad/statement-by-members/combined-all-csv/"
-        data = pd.read_csv(datapath+filename,delimiter=',' ,header=0, names=['speechtext'])
+        data = self.read_csv(datapath,filename)
+        #Removing all panctuations from speech text
         speechtext = data.speechtext.str.replace(pattern, '')
 
-
+        #Using tf idf to find words or tokens that are less important
         vectorizer = TfidfVectorizer(decode_error='replace', token_pattern=r'(?u)\b[A-Za-z]+\b',stop_words='english',encoding='utf-8',ngram_range=(1,2))
         tfidf = vectorizer.fit_transform(speechtext)
 
         terms = vectorizer.get_feature_names()
-
         sums = tfidf.sum(axis=0)
         data = []
         for col, term in enumerate(terms):
@@ -40,6 +44,6 @@ class Preprocessing():
         print(ranking.sort_values('rank', ascending=False).nlargest(25, 'rank'))
 
 if __name__ == '__main__':
-    Preprocessing()
+     Preprocessing()
 
 
