0e874414437600e989e0a8f9cbfafe4641bf9f32
Ruta Hareshbhai Bakhda <ruta.hareshbhai.bakhda@uni-weimar.de>
2019-05-07 11:26:42 +0200

    Update remove-cliche.py

--- data-preprocessing/remove-cliche.py
+++ data-preprocessing/remove-cliche.py
@@ -12,20 +12,36 @@ pattern = r"[{}]".format(rem)
 
 class Preprocessing():
     basepath = os.path.dirname(os.path.abspath(__file__))
+    # Datapath for Statement by Members
     datapath_sbm = basepath + "/lipad/statement-by-members/combined-all-csv/"
-    datapath_oq = basepath + "/lipad/statement-by-members/combined-all-csv/"
+    #Datapath for Oral Questions
+    datapath_oq = basepath + "/lipad/oral-questions/combined-all-csv/"
 
     def __init__(self):
         super().__init__()
         #task 1 : find most common words
-        self.find_cliche(self.datapath_sbm,'combined.csv')
-        self.find_cliche(self.datapath_oq,'combined.csv')
+        #self.find_cliche(self.datapath_sbm,'combined.csv')
+        #self.find_cliche(self.datapath_oq,'combined.csv')
+
+        #Task 2 : Report 10 most occuring subtopic in Oral Questions and Statement by Members
+        print("Subtopic 10 most occurence for Oral Questions")
+        self.subtopic_most_count(self.datapath_oq,'combined-oral-questions.csv')
+        print("Subtopic 10 most occurence for Statement by Members")
+        self.subtopic_most_count(self.datapath_sbm,'combined-statement-by-members.csv')
+
 
     def read_csv(self,datapath,filename):
+        """ Reading the csv file from given path """
         read_csv_data = pd.read_csv(datapath+filename,delimiter=',' ,header=0)
         return read_csv_data
 
     def find_cliche(self,datapath,filename):
+        """ Calculates the most common words (cliches) in csv
+        Removes the punctuations from csv
+        TF-IDF to calculate most common words
+        Extracting most common words
+
+        """
         data = self.read_csv(datapath,filename)
         #Removing all panctuations from speech text
         speechtext = data.speechtext.str.replace(pattern, '')
@@ -42,6 +58,13 @@ class Preprocessing():
 
         ranking = pd.DataFrame(data, columns=['term','rank'])
         print(ranking.sort_values('rank', ascending=False).nlargest(25, 'rank'))
+
+    def subtopic_most_count(self,datapath,filename):
+        """" Reading csv and finding 10 subtopics with most occurence """
+        data = self.read_csv(datapath, filename)
+        print(data['subtopic'].value_counts().nlargest(10))
+
+
 
 if __name__ == '__main__':
      Preprocessing()
