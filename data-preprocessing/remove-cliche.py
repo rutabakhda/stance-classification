500a6441b95aece4ff221e54177d4261c69aa607
Ruta Hareshbhai Bakhda <ruta.hareshbhai.bakhda@uni-weimar.de>
2019-05-07 11:48:09 +0200

    Update remove-cliche.py

--- data-preprocessing/remove-cliche.py
+++ data-preprocessing/remove-cliche.py
@@ -21,11 +21,25 @@ class Preprocessing():
         self.find_cliche(self.datapath_sbm,'combined.csv')
         self.find_cliche(self.datapath_oq,'combined.csv')
 
-        #Task 2 : Report 10 most occuring subtopic in Oral Questions and Statement by Members
+        #Task 2 : Report 10 most occurring subtopic in Oral Questions and Statement by Members
         print("Subtopic 10 most occurence for Oral Questions")
         self.subtopic_most_count(self.datapath_oq,'combined-oral-questions.csv')
         print("Subtopic 10 most occurence for Statement by Members")
         self.subtopic_most_count(self.datapath_sbm,'combined-statement-by-members.csv')
+
+        #Task 3 : Finding controversial topics in Oral Questions and Statement by Members
+        self.outname = 'controversial-data.csv'
+        self.fullname_oq = os.path.join(self.datapath_oq, self.outname)
+        self.find_controversial_topics(self.datapath_oq, 'combined-oral-questions.csv', self.fullname_oq)
+
+        self.fullname_sbm = os.path.join(self.datapath_sbm, self.outname)
+        self.subtopic_most_count(self.datapath_sbm, 'combined-statement-by-members.csv',self.fullname_sbm)
+
+        #Task 4 : Report 10 most occurring subtopics in Controversial Oral Questions and Statement by Members
+        print("Subtopic 10 most occurence for Controversial Oral Questions")
+        self.subtopic_most_count(self.datapath_oq, 'controversial-data.csv')
+        print("Subtopic 10 most occurence for Controversial Statement by Members")
+        self.subtopic_most_count(self.datapath_sbm, 'controversial-data.csv')
 
 
     def read_csv(self,datapath,filename):
@@ -61,6 +75,34 @@ class Preprocessing():
         data = self.read_csv(datapath, filename)
         print(data['subtopic'].value_counts().nlargest(10))
 
+    def find_controversial_topics(self,datapath,filename,fullname):
+        read_data = self.read_csv(datapath, filename)
+        subtopic = read_data.subtopic.notnull()
+        data = read_data[subtopic]
+
+        data['lower_subtopic'] = data['subtopic'].str.lower()
+        data['lower_speechtext'] = data['speechtext'].str.lower()
+
+        name = "wikipedia-controversial-issues - Copy.txt"
+        with open(name, 'r', encoding='utf8') as f:
+            for line in f:
+                # for word in line.split():
+                line = line.lower()
+                line = line.strip()
+                print(line)
+
+                new_data = data[data['lower_subtopic'].str.contains(line)]
+                filtered_new_data = data[data['lower_speechtext'].str.count(line) > 1]
+                filtered_new_data = filtered_new_data.drop(['lower_speechtext', 'lower_subtopic'], axis=1)
+                # print(filtered_new_data)
+
+                if os.path.isfile(fullname):
+                    with open(fullname, 'a', encoding='utf-8') as f:
+                        filtered_new_data.to_csv(f, header=False, index=False)
+
+                else:
+                    with open(fullname, 'a', encoding='utf-8') as f:
+                        filtered_new_data.to_csv(f, index=False)
 
 
 if __name__ == '__main__':
