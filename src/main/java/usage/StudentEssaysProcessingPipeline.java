b44ad771563af4c60f3e629591dadb22cb925e30
Lukas Trautner <lukas.peter.trautner@uni-weimar.de>
2019-06-04 18:15:11 +0200

    add input and output directory paths for student essay pipeline

--- src/main/java/usage/StudentEssaysProcessingPipeline.java
+++ src/main/java/usage/StudentEssaysProcessingPipeline.java
@@ -7,6 +7,11 @@ package usage;
  */
 public class StudentEssaysProcessingPipeline {
 	
+	private static final String INPUT_COLLECTION_DIR = "data/student-essays/original";
+
+	private static final String ANALYSIS_ENGINE_PATH = "src/main/resources/uima/aggregates/PosTokenTagger.xml";
+
+	private static final String OUTPUT_COLLECTION_DIR = "data/student-essays/processed";
 	
 	
 	public void processCollection() {
