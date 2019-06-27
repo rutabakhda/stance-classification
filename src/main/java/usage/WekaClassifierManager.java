03bf0a99ef2bf179a950496b9ee4906a5cb6503e
Annie Anh Phuong <aphuongle95@gmail.com>
2019-06-28 00:00:10 +0200

    update classifier to see predicted vs actual value

--- src/main/java/usage/WekaClassifierManager.java
+++ src/main/java/usage/WekaClassifierManager.java
@@ -1,84 +1,127 @@
 package usage;
 
+import java.io.BufferedReader;
 import java.io.File;
-import java.sql.Timestamp;    
-import java.util.Date;    
-import java.text.SimpleDateFormat;  
-import java.util.ArrayList;
-import java.util.List;
-import java.util.regex.Pattern;
+import java.io.FileReader;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Date;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import weka.classifiers.Classifier;
 import weka.classifiers.Evaluation;
 import weka.classifiers.trees.RandomForest;
+import weka.core.Instance;
 import weka.core.Instances;
 import weka.core.converters.ConverterUtils.DataSource;
-import de.aitools.ie.uima.io.WekaFeatureFileWriter;
 
 public class WekaClassifierManager {
 
-	private static final String TRAINING_SET_PATH = "data/cross-domain/debatepedia_sample-sbm/arff/content-length_pos-ngrams_token-ngrams_2019-06-14_data-debatepedia-processed.arff";
+	private Classifier classifier;
+	private Instances trainingInstances;
+	private Instances testingInstances;
 
-	/**
-	 * The path of the feature file (see {@link WekaFeatureFileWriter}) used for
-	 * testing the classifier.
-	 */
-	private static final String TEST_SET_PATH = "data/cross-domain/debatepedia_sample-sbm/arff/content-length_pos-ngrams_token-ngrams_2019-06-14_data-sample-sbm-processed.arff";
-	// -------------------------------------------------------------------------
-	// REFERENCES
-	// -------------------------------------------------------------------------
+	static String TRAINING_SET_PATH = "data/cross-domain/debatepedia_sample-sbm/arff/content-length_pos-ngrams_token-ngrams_2019-06-14_data-debatepedia-processed.arff";
+	static String TEST_SET_PATH = "data/cross-domain/debatepedia_sample-sbm/arff/content-length_pos-ngrams_token-ngrams_2019-06-14_data-sample-sbm-processed.arff";
 
-	/**
-	 * The classifier provided in the constructor.
-	 */
-	private final Classifier classifier;
+	private static final String INPUT_FILE = "data/sample-sbm/json/original.json";
+	private JSONArray originalData;
 
-	/**
-	 * Patterns of names of attributes to be removed from training and test set.
-	 */
-	private final List<Pattern> attributeIgnorePatterns;
+	private JSONArray getOriginalData() {
 
-	/**
-	 * The instances read from the feature file provided in the constructor.
-	 */
-	private final Instances trainingInstances;
+		File inputFile = new File(INPUT_FILE);
+		System.out.println("original file: " + inputFile.getAbsolutePath());
+		JSONArray originalInstances = new JSONArray();
+		String json = this.readJsonFile(inputFile);
 
-	// -------------------------------------------------------------------------
-	// INITIALIZATION
-	// -------------------------------------------------------------------------
+		if (!json.equals("")) {
+			originalInstances = new JSONArray(json);
+		}
 
-	/**
-	 * The constructor creates and trains a new {@link RandomForest} classifier with
-	 * the provided feature file.
-	 * 
-	 * @param trainingSetFeatureFile The features used for training the classifier.
-	 * @throws Exception If the file could not be read or the classifier not be
-	 *                   trained.
-	 */
-	public WekaClassifierManager(File trainingSetFeatureFile) throws Exception {
-		this(new RandomForest(), new ArrayList<Pattern>(), trainingSetFeatureFile);
+		return originalInstances;
 	}
 
-	/**
-	 * The constructor trains the provided classifier with the provided feature
-	 * file.
-	 * 
-	 * @param classifier              The classifier to use.
-	 * @param attributeIgnorePatterns Patterns of names of attributes to be removed
-	 *                                from training and test set.
-	 * @param trainingSetFeatureFile  The feature vectors used for training the
-	 *                                classifier.
-	 * @throws Exception If the file could not be read or the classifier not be
-	 *                   trained.
-	 */
-	public WekaClassifierManager(Classifier classifier, Iterable<Pattern> attributeIgnorePatterns,
-			File trainingSetFeatureFile) throws Exception {
-		this.classifier = classifier;
-		this.attributeIgnorePatterns = new ArrayList<>();
-		for (final Pattern pattern : attributeIgnorePatterns) {
-			this.attributeIgnorePatterns.add(pattern);
-		}
+	public WekaClassifierManager(String trainingFeaturesFilePath, String testFeaturesFilePath) throws Exception {
+
+		File trainingSetFeatureFile = new File(trainingFeaturesFilePath);
+		File testFeatureFile = new File(testFeaturesFilePath);
+
 		this.trainingInstances = this.readInstances(trainingSetFeatureFile);
+		this.testingInstances = this.readInstances(testFeatureFile);
+
+		this.classifier = new RandomForest();
+
+		this.originalData = this.getOriginalData();
+
+	}
+
+	private String readJsonFile(File inputFile) {
+		String content = "";
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(inputFile));
+			String line = reader.readLine();
+			while (line != null) {
+				content = content + line;
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (Exception e) {
+			throw new RuntimeException();
+		}
+		return content;
+	}
+
+	public static void main(String[] args) throws Exception {
+		String trainingSetPath = TRAINING_SET_PATH;
+		String testSetPath = TEST_SET_PATH;
+		if (args.length > 0 && args[0].length() > 0) {
+			trainingSetPath = args[0];
+
+		}
+
+		if (args.length > 1 && args[1].length() > 0) {
+			testSetPath = args[1];
+		}
+
+		classify(trainingSetPath, testSetPath);
+	}
+
+	public static void classify(String trainingFeaturesFilePath, String testFeaturesFilePath) throws Exception {
+		boolean needTrain = true;
+
+		String modelFilePath = "";
+		String modelFileFolder = new File(trainingFeaturesFilePath).getParentFile().getParent() + "/" + "models";
+		if (needTrain) {
+			modelFilePath = modelFileFolder + "/" + "model-" + getDateTime();
+		} else {
+			File[] allModelFiles = new File(modelFileFolder).listFiles();
+			Arrays.sort(allModelFiles, new Comparator<File>() {
+				@Override
+				public int compare(File f1, File f2) {
+					return Long.compare(f1.lastModified(), f2.lastModified());
+				}
+			});
+			modelFilePath = allModelFiles[0].getAbsolutePath();
+		}
+
+		WekaClassifierManager manager = new WekaClassifierManager(trainingFeaturesFilePath, testFeaturesFilePath);
+
+		File trainingSetFeatureFile = new File(trainingFeaturesFilePath);
+
+		manager.trainingInstances = manager.readInstances(trainingSetFeatureFile);
+
+		manager.buildClassifier(trainingSetFeatureFile, modelFilePath);
+
+		// manager.classifier = (Classifier) SerializationHelper.read(modelFilePath);
+		File testSetFeatureFile = new File(testFeaturesFilePath);
+		manager.testAndPrint(testSetFeatureFile);
+	}
+
+	private void buildClassifier(File trainingSetFeatureFile, String modelFilePath) throws Exception {
 		// Train classifier
 		System.out.println("=== Start ===");
 		System.out.println(
@@ -86,51 +129,48 @@ public class WekaClassifierManager {
 						+ " instances with " + this.trainingInstances.numAttributes() + " attributes.");
 		this.classifier.buildClassifier(this.trainingInstances);
 		System.out.println("Built: " + this.classifier);
-		String modelFileFolder = trainingSetFeatureFile.getParentFile().getParent() + "/" + "models";
-		String modelFilePath = modelFileFolder + "/" + "model-" + this.getDateTime();              
+
 		// Save model
 		weka.core.SerializationHelper.write(modelFilePath, classifier);
 	}
-	
-	private String getDateTime(){    
-        Date date = new Date();  
-        Timestamp ts=new Timestamp(date.getTime());  
-        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");  
-        return formatter.format(ts);                     
-	}
 
-	// -------------------------------------------------------------------------
-	// CLASSIFICATION METHODS
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Classify the feature vectors in the provided file and return an evaluation
-	 * result object.
-	 * 
-	 * @param testSetFeatureFile The feature vectors used to evaluate the
-	 *                           classifier.
-	 * @return The evaluation result object.
-	 * @throws Exception If the file could not be read or the feature vectors could
-	 *                   not be classified.
-	 */
-	public Evaluation test(File testSetFeatureFile) throws Exception {
-		Instances testInstances = this.readInstances(testSetFeatureFile);
-		System.out.println("Evaluate on " + testInstances.size() + " instances");
+	private Evaluation test(File testSetFeatureFile) throws Exception {
+		System.out.println("Evaluate on " + this.testingInstances.size() + " instances");
+		System.out.println();
 		Evaluation evaluation = new Evaluation(this.trainingInstances);
-		evaluation.evaluateModel(this.classifier, testInstances);
+		evaluation.evaluateModel(this.classifier, this.testingInstances);
+
+		System.out.println("Tracing back to original data ...");
+		System.out.println("Format: sentence" + "\n" + "actual unit type, predicted unit type \n");
+		int countIncorrectPremise = 0;
+		int countIncorrectConclusion = 0;
+
+		for (int i = 0; i < this.testingInstances.size(); i++) {
+			Instance instance = this.testingInstances.get(i);
+			double prediction = this.classifier.classifyInstance(instance);
+			String actualString = instance.classAttribute().value((int) instance.classValue());
+			String predictedString = instance.classAttribute().value((int) prediction);
+
+			if (!actualString.contentEquals(predictedString)) {
+				JSONObject originalJsonObject = this.originalData.getJSONObject(i);
+				System.out.println(originalJsonObject.getString("content") + "\n"
+						+ originalJsonObject.getString("unitType") + " , " + predictedString + "\n");
+				if (actualString.contentEquals("premise")) {
+					countIncorrectPremise++;
+				} else if (actualString.contentEquals("conclusion")) {
+					countIncorrectConclusion++;
+				}
+			}
+		}
+
+		System.out.println("===SUMMARY===");
+		System.out.println("Number of incorrected labeled premise   : " + String.valueOf(countIncorrectPremise));
+		System.out.println("Number of incorrected labeled conclusion: " + String.valueOf(countIncorrectConclusion));
+		System.out.println("=============");
+
 		return evaluation;
 	}
 
-	/**
-	 * Classify the feature vectors in the provided file and print a summarization
-	 * of the classifier and the results to standard output.
-	 * 
-	 * @param testSetFeatureFile The feature vectors used to evaluate the
-	 *                           classifier.
-	 *
-	 * @throws Exception If the file could not be read or the feature vectors could
-	 *                   not be classified.
-	 */
 	public void testAndPrint(File testSetFeatureFile) throws Exception {
 		Evaluation evaluation = this.test(testSetFeatureFile);
 		System.out.println();
@@ -142,91 +182,20 @@ public class WekaClassifierManager {
 		System.out.println(evaluation.toMatrixString());
 	}
 
-	// -------------------------------------------------------------------------
-	// INTERNAL HELPERS
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Read the feature vectors in given file.
-	 * 
-	 * @param featureFile The file that contains the feature vectors.
-	 * @return The read feature vectors.
-	 * @throws Exception If the file could not be read.
-	 */
-	protected Instances readInstances(File featureFile) throws Exception {
+	private Instances readInstances(File featureFile) throws Exception {
 		DataSource source = new DataSource(featureFile.getAbsolutePath());
 		Instances instances = source.getDataSet();
 		if (instances.classIndex() == -1) {
 			instances.setClassIndex(instances.numAttributes() - 1);
 		}
 
-		if (!this.attributeIgnorePatterns.isEmpty()) {
-			for (int a = instances.numAttributes() - 1; a >= 0; --a) {
-				if (a == instances.classIndex()) {
-					continue;
-				}
-
-				String attributeName = instances.attribute(a).name();
-				for (Pattern pattern : this.attributeIgnorePatterns) {
-					if (pattern.matcher(attributeName).matches()) {
-						instances.deleteAttributeAt(a);
-						break;
-					}
-				}
-			}
-		}
-
 		return instances;
 	}
 
-	// -------------------------------------------------------------------------
-	// MAIN
-	// -------------------------------------------------------------------------
-
-	/**
-	 * Performs a simple training-and-test-set evaluation with a
-	 * {@link RandomForest} classifier and prints a summary of the results to
-	 * standard output.
-	 * 
-	 * @param args Either nothing (to use the {@link #TRAINING_SET_PATH} and
-	 *             {@link #TEST_SET_PATH}) or
-	 *             <P>
-	 *             <CODE>
-	 * training.arff test.arff [ignore-attribute [ignore-attribute] [...]]
-	 * </CODE>
-	 *             </P>
-	 *             Where <CODE>ignore-attribute</CODE> are optional regular
-	 *             expression patterns that match the attribute names that should be
-	 *             ignored.
-	 * @throws Exception If a file could not be read or the classifier could not be
-	 *                   trained or evaluated.
-	 */
-	public static void classify(String trainingFeaturesFilePath, String testFeaturesFilePath) throws Exception {
-		Classifier forest = new RandomForest();
-
-		List<Pattern> attributeIgnorePatterns = new ArrayList<>();
-
-		File trainingSetFeatureFile = new File(trainingFeaturesFilePath);
-		WekaClassifierManager classifier = new WekaClassifierManager(forest, attributeIgnorePatterns,
-				trainingSetFeatureFile);
-
-		File testSetFeatureFile = new File(testFeaturesFilePath);
-		classifier.testAndPrint(testSetFeatureFile);
+	private static String getDateTime() {
+		Date date = new Date();
+		Timestamp ts = new Timestamp(date.getTime());
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		return formatter.format(ts);
 	}
-
-	public static void main(String[] args) throws Exception {
-
-		String trainingSetPath = TRAINING_SET_PATH;
-		String testSetPath = TEST_SET_PATH;
-
-		if (args.length > 0 && args[0].length() > 0) {
-			trainingSetPath = args[0];
-		}
-		if (args.length > 1 && args[1].length() > 0) {
-			testSetPath = args[1];
-		}
-		classify(trainingSetPath, testSetPath);
-
-	}
-
 }
